7fca486bece1efe16b757f7ebaf37b8c61b2e6c3
tiagobr21 <tiagobrr17@gmail.com>
2023-02-19 16:58:45 -0300

    fron-end started

--- src/main/java/authentication/authentication/AuthenticationApplication.java
+++ src/main/java/authentication/authentication/AuthenticationApplication.java
@@ -1,7 +1,16 @@
 package authentication.authentication;
 
+
+import com.sun.faces.config.ConfigureListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import javax.faces.webapp.FacesServlet;
+
 
 @SpringBootApplication
 public class AuthenticationApplication {
@@ -9,5 +18,33 @@ public class AuthenticationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationApplication.class, args);
 	}
+
+
+	@Bean
+	public ServletRegistrationBean facesServletRegistration() {
+		ServletRegistrationBean registration = new ServletRegistrationBean<>(new FacesServlet(), "*.xhtml");
+		registration.setLoadOnStartup(1);
+		registration.addUrlMappings("*.jr");
+		return registration;
+	}
+
+	@Bean
+	public ServletContextInitializer servletContextInitializer() {
+		return servletContext -> {
+			servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+			servletContext.setInitParameter("primefaces.THEME", "redmond");
+		};
+	}
+
+	@Bean
+	public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
+		return new ServletListenerRegistrationBean<>(new ConfigureListener());
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
 
 }
