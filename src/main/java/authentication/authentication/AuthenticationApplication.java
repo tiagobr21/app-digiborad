8d2b1f02beb315f79dd0ae6986b2d5079c4e01be
tiagobr21 <tiagobrr17@gmail.com>
2023-02-20 15:40:31 -0300

    delete and refresh ok

--- src/main/java/authentication/authentication/AuthenticationApplication.java
+++ src/main/java/authentication/authentication/AuthenticationApplication.java
@@ -5,10 +5,11 @@ import com.sun.faces.config.ConfigureListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 import javax.faces.webapp.FacesServlet;
 
 
@@ -40,6 +41,7 @@ public class AuthenticationApplication {
 	public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
 		return new ServletListenerRegistrationBean<>(new ConfigureListener());
 	}
+
 
 	@Bean
 	public RestTemplate restTemplate() {
--- src/main/java/authentication/authentication/modules/user/Beans/UsersBN.java
+++ src/main/java/authentication/authentication/modules/user/Beans/UsersBN.java
@@ -3,6 +3,7 @@ package authentication.authentication.modules.user.Beans;
 
 import authentication.authentication.modules.user.entities.User;
 import authentication.authentication.modules.user.repository.UserRepository;
+import authentication.authentication.modules.user.services.CreateUserService;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +24,9 @@ public class UsersBN {
     @Autowired
     private UserRepository userRepository;
 
+    @Inject
+    private CreateUserService createUserService;
+
     @Getter
     @Setter
     private List<User> users = new ArrayList<>();
@@ -31,10 +36,20 @@ public class UsersBN {
     private User user;
 
 
-    @PostConstruct
-    public List<User> getListAll() {
+    private List<User> userslist;
+    public void onload(){
+        this.userslist = userRepository.findAll();
+    }
+
+   @PostConstruct
+    public List<User> list() {
         users = userRepository.findAll();
         return users;
+    }
+
+    public String delete(User user){
+        createUserService.delete(user);
+        return "index?faces-redirect=true";
     }
 
     public Integer getListSize() {
